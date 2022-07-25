a65c66d9697cac2efe53a56561f5684792e86b42
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-07-25 03:36:56 +0000

    added comment on 846 value

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,6 +6,9 @@ import PathFinder from "./views/PathFinder"
 function App() {
   // canvasSize will set the number of Nodes
   // to render
+
+  // 846 is a random value that happens to fill
+  // canvas
   return (
     <div id="App">
       <div id="header">
