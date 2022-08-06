512bd0e3f192d264fece9ddb577cbc9c60933c91
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-08-06 18:52:47 +0000

    finished with Dijkstra algorithm(my version of it)

--- app/src/graphs/RowNodeGraph.ts
+++ app/src/graphs/RowNodeGraph.ts
@@ -1,4 +1,4 @@
-import { RowNode } from "../components/Row";
+import { Row, RowNode } from "../components/Row";
 
 type NodeGraph = {
     [key : number] : (RowNode | null)[]
@@ -7,9 +7,13 @@ type NodeGraph = {
 export default class RowNodeGraph{
     nodes : NodeGraph
     NumberOfNodes : number
-    constructor(){
+    nodesToVisit : RowNode[]
+    endNodeFound : boolean
+    constructor(begginnerNode : RowNode){
         this.NumberOfNodes = 0
         this.nodes = {}
+        this.nodesToVisit = [begginnerNode]
+        this.endNodeFound = false
     }
 
     public addNode(rowNode : RowNode){
@@ -17,6 +21,8 @@ export default class RowNodeGraph{
         this.NumberOfNodes = this.NumberOfNodes + 1
     }
 
+    // every node will have 4 members in its array, they may be
+    // of type RowNode or null
     public addEdge(rowNode : RowNode){
         let upperRowNode : RowNode | null = null
         let bottomRowNode : RowNode | null = null
@@ -32,11 +38,43 @@ export default class RowNodeGraph{
             // of rowNode but logically it is in nextRow
             bottomRowNode = rowNode.row.NextRow.Nodes[rowNode.id+lengthOfNodes]
         }
-        this.nodes[rowNode.id] = [
-            rowNode.previousRowNode,
-            upperRowNode,
-            rowNode.nextRowNode,
-            bottomRowNode
-        ]
+        this.nodes[rowNode.id][0] = upperRowNode
+        this.nodes[rowNode.id][1] = rowNode.previousRowNode
+        this.nodes[rowNode.id][2] = bottomRowNode
+        this.nodes[rowNode.id][3] = rowNode.nextRowNode
+    }
+
+    public Dijkstra(nodes : RowNode[]) : RowNode[]{
+        // make a copy of nodes so any changes on the go
+        // do not reflect on the nodes
+        let nodesCopy : RowNode[] = structuredClone(nodes)
+        // for every node in this.nodesToVisit we create a new edge
+        // in the graph and at the same time we check if it has been
+        // visited, if it hasn't then we change the value isVisited
+        // to true, we then loop through the edge of this node
+        // and for every node in that edge we add that node to newNodesToVisit
+        // and at the end we return the copy of nodes and set newNodesToVisit
+        // as this.nodesToVisit and if we found a node that is endNode
+        // we set endNodeFound to true
+        let newNodesToVisit : RowNode[] = []
+        this.nodesToVisit.map((node) => {
+            if(!node.isBeginning && !node.isVisited && !node.isEnd){
+                nodesCopy[node.id].isVisited = true
+            }
+            if(node.isEnd){
+                this.endNodeFound = true
+                return nodesCopy
+            }
+            this.addNode(node)
+            this.addEdge(node)
+            let currentGraphNode : (RowNode|null)[] = this.nodes[node.id]
+            currentGraphNode.map((node) => {
+                if(node){
+                    newNodesToVisit.push(node)
+                }
+            })
+        })
+        this.nodesToVisit = newNodesToVisit
+        return nodesCopy
     }
 }
